cc6c5c90ded93b35fb678d99a15f9c08efb9cf7e
jafow <jared.a.fowler@gmail.com>
2018-12-23 01:58:41 -0800

    Build chart with word and style opacity based on word
    more content
    
    update text

--- main.js
+++ main.js
@@ -10,18 +10,16 @@ d3.csv('./public/data/bubbles.csv')
       freq_t: r.freq * (10 ** 5)
     }))
     makeBubbles(rowFreq, '#frame-all')
-    highlightLove (rowFreq, '#frame-love', loveFilter)
+    highlightLove(rowFreq.slice(0, 35), '#frame-love', loveFilter)
   })
   .catch(function (err) {
     console.error(new Error(err))
   })
 
-
 const height = 840
 const width = 1080
 const margin = {left: 20, right: 20, horiz: 40, vert: 40}
 const color = d3.scaleOrdinal().range(d3.schemeCategory10)
-
 
 function makeBubbles (rows, elId, colorFilter=identity) {
   const g = setupSvg(rows, elId)
@@ -37,14 +35,29 @@ function makeBubbles (rows, elId, colorFilter=identity) {
 }
 
 function highlightLove (rows, elId, colorFilter) {
-  const g = setupSvg(rows, elId)
+  const g = setupSvg(rows.map(r => Object.assign(r, {freq_t: r.freq * 50000})), elId)
 
-    g.append('circle')
+  g.append('circle')
       .attr('r', d => d.freq_t)
       .attr('cx', (d) => d.word === 'love' ? (width + margin.horiz) / 5 : Math.random() * width)
       .attr('cy', (d) => d.word === 'love' ? (height + margin.vert) / 2.5 : Math.random() * height)
+      .attr('data-coord', function (d) { d.coord = {_x: this.cx.baseVal.value, _y: this.cy.baseVal.value}})
       .attr('fill-opacity', d => d.word === 'love' ? 1 : 0.36)
+      .attr('stroke', d => d.word in {'love': 1, 'work': 1, 'got': 1} ? '#000' : '')
       .attr('fill', d => color(colorFilter(d.word)))
+      .on('mouseenter', function (d) {
+        var x = this.cx.baseVal.value
+        console.log(`base val ${x}`)
+      })
+
+  g.append('text')
+      .append('tspan')
+      .attr('class', 'label')
+      .attr('fill', '#fff')
+      .attr('fill-opacity', d => d.word in {'love': 1, 'work': 1, 'got': 1}  ? 1 : 0.0)
+      .attr('x', d => d.coord._x - 22)
+      .attr('y', d => d.coord._y)
+      .text(d => d.word)
 
     return g 
 }
